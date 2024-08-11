4c1b033949f23a50d4c28e653a008affa8009d7c
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-08-11 17:03:25 -0600

    IMPORTANTE creación del pageConfig <------

--- readme.txt
+++ readme.txt
@@ -5,8 +5,9 @@ Los assets y componentes están separados de la lógica del renderizado
 El layout.tsx es el elemento que se va a renderizar a partir de la ruta actuál
 El layout.tsx es heredado por las rutas hijas
 El page.tsx es el elemento que se va a renderizar en la ruta actuál
-1._ Importar todos los archivos .sass a /src/app/global.scss
 Configura los objetos de configuración para cada componente de estilos.
 Asegura usar entidades para caracteres especiales. Ejemplo: á: &aacute;
 Las propiedades de elementos que son arreglos como contentOfFixProblemInnerSection1.cards ya tienen el número ideal de elementos a renderizar
 Sin embargo en algunos casos se seguiran viendo bien si añadimos más elementos y en otros casos no.
+1._ Importar todos los archivos .sass a /src/app/global.scss
+2._ Genera componentes en /pageConfig que funcionen como páginas independientes.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,9 @@
-'use client'
-import { BenefitsSection } from "@/styledComponents/benefitsSection/BenefitsSection";
-import { CauseSection } from "@/styledComponents/causeSection/CauseSection";
-import { ContactSection } from "@/styledComponents/contactSection/ContactSection";
-import { FixProblemInnerSection1 } from "@/styledComponents/fixProblemInnerSection1/FixProblemInnerSection1";
-import { FixProblemInnerSection2 } from "@/styledComponents/fixProblemInnerSection2/FixProblemInnerSection2";
-import { FixProblemSection } from "@/styledComponents/fixProblemSection/FixProblemSection";
-import { Footer } from "@/styledComponents/footer/Footer";
-import { HeroSection } from "@/styledComponents/heroSection/HeroSection";
-import { LogoSection } from "@/styledComponents/logoSection/LogoSection";
-import { Navbar } from "@/styledComponents/navbar/Navbar";
-import { ProblemSection } from "@/styledComponents/problemSection/ProblemSection";
-import { TestimonialSection } from "@/styledComponents/testimonialSection/TestimonialSection";
-import { useEffect } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';  // Importa los estilos de AOS
-import '@splidejs/splide/dist/css/splide.min.css';
-
-
+import { YourPage1 } from "@/pageConfig/YourPage1";
 
 export default function Home() {
-  useEffect(()=>{
-    AOS.init();
-  },[]);
-  useEffect(() => {
-    const handleScroll = (): void => {
-      const sections = document.querySelectorAll<HTMLElement>('section');
-      const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-      sections.forEach((section) => {
-        if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetTop + section.offsetHeight) {
-          section.classList.add('active');
-        } else {
-          section.classList.remove('active');
-        }
-      });
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
-  
-
-
   return (
     <>
-      {/* <Navbar/> */}
-      <HeroSection/>
-      <LogoSection/>
-      {/* <ProblemSection/> */}
-      {/* <CauseSection /> */}
-      {/* <FixProblemSection/> */}
-      <FixProblemInnerSection1/>
-      <FixProblemInnerSection2/>
-      <TestimonialSection/>
-      {/* <BenefitsSection/> */}
-      {/* <ContactSection/> */}
-      <Footer/>
+      <YourPage1/>
     </>
   )
 }
